69c4fab51c26eb0a35df2fc514cd09b160602a28
Clemo97 <lumumbaclement@gmail.com>
2022-04-25 20:59:54 +0300

    test find credentials

--- passlock_test.py
+++ passlock_test.py
@@ -72,5 +72,39 @@ class TestCredentials(unittest.TestCase):
         test_credential.save_details()
         self.assertEqual(len(Credentials.credentials_list),2)
 
+    def test_delete_credential(self):
+        """
+        test method to test if we can remove an account credentials from our credentials_list
+        """
+        self.new_credential.save_details()
+        test_credential = Credentials("Twitter", "mikeycharles", "Mfh45hfk")
+        test_credential.save_details()
+
+        self.new_credential.delete_credentials()
+        self.assertEqual(len(Credentials.credentials_list), 1)
+
+    def test_find_credentialr(self):
+        """
+        test to check if we can find a credential entry by account name and display the details of the credential
+        """
+        self.new_credential.save_details()
+        test_credential = Credentials("Twitter", "mikeycharles", "Mfh45hfk")
+        test_credential.save_details()
+
+        the_credential = Credentials.find_credential("Twitter")
+
+        self.assertEqual(the_credential.account, test_credential.account)
+
+    def test_credential_exist(self):
+        """
+        test to check if we can return a true or false based on whether we find or can't find the credential.
+        """
+        self.new_credential.save_details()
+        the_credential = Credentials("Twitter", "mikeycharles", "Mfh45hfk")
+        the_credential.save_details()
+        credential_is_found = Credentials.if_credential_exist("Twitter")
+        self.assertTrue(credential_is_found)
+
+
 
 
