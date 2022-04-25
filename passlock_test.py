58bd69f31a7f5485ac53823a3c220857e1c9eff6
Clemo97 <lumumbaclement@gmail.com>
2022-04-25 20:59:15 +0300

    test save credentials

--- passlock_test.py
+++ passlock_test.py
@@ -49,4 +49,28 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credential.userName, 'Owiti_Charles')
         self.assertEqual(self.new_credential.password, 'yx5Gij43')
 
+    def save_credential_test(self):
+        """
+        test case to test if the crential object is saved into the credentials list.
+
+        """
+        self.new_credential.save_details()
+        self.assertEqual(len(Credentials.credentials_list),1)
+
+    def tearDown(self):
+        '''
+        method that does clean up after each test case has run.
+        '''
+        Credentials.credentials_list = []
+
+    def test_save_many_accounts(self):
+        '''
+        test to check if we can save multiple credentials objects to our credentials list
+        '''
+        self.new_credential.save_details()
+        test_credential = Credentials("Twitter","mikeycharles","Mfh45hfk") 
+        test_credential.save_details()
+        self.assertEqual(len(Credentials.credentials_list),2)
+
+
 
